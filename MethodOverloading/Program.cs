a9301e711e21262d6bc87a7f9afcdd47b4521513
George Harris <142560344+GeorgeHarris86@users.noreply.github.com>
2023-11-17 14:14:53 -0500

    Update Program.cs
    pascal casing errors fixed

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -6,10 +6,10 @@ methods.Add(3,5);
 
 
 var answer = methods.add(3, 5);
-console.writeline(answer);
+Console.WriteLine(answer);
 
 var answer2 = methods.add(1.5m, 5.6m);
-console.writeline(answer2);
+Console.WriteLine(answer2);
 
 var dollars = methods.Add(0,1,true);
 Console.WriteLine(dollars);
